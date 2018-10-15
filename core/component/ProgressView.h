d6bc85b016bcf8f91df3208e75983621cf9b0cf2
Fu Jiaming <478560836@163.com>
2018-10-15 16:42:01 +0800

    add progress view

---
+++ core/component/ProgressView.h
@@ -0,0 +1,17 @@
+//
+//  ProgressView.h
+//  FloraCore
+//
+//  Created by Fu Jiaming on 2018/10/15.
+//  Copyright © 2018年 Fu Jiaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProgressView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ core/component/ProgressView.m
@@ -0,0 +1,21 @@
+//
+//  ProgressView.m
+//  FloraCore
+//
+//  Created by Fu Jiaming on 2018/10/15.
+//  Copyright © 2018年 Fu Jiaming. All rights reserved.
+//
+
+#import "ProgressView.h"
+
+@implementation ProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
